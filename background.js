7556447ade3e43e54738eb9731eff5d1edac698b
Giulio Malventi <giulio.malventi@gmail.com>
2023-12-12 22:14:19 +0100

    Avoid close listeners

--- background.js
+++ background.js
@@ -1,35 +1,33 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.onClicked.addListener(tab => {
-    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-      chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        function: redirect,
-        args: [tabs[0].url],
-      })
-    })
-  })
-
   chrome.contextMenus.create({
     id: 'redirectMenuItem',
     title: 'Open in JSCAD',
     contexts: ['link'],
   })
+})
 
-  chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === 'redirectMenuItem') {
-      chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        function: redirect,
-        args: [info.linkUrl],
-      })
-    }
+chrome.action.onClicked.addListener(tab => {
+  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      function: redirect,
+      args: [tabs[0].url],
+    })
   })
+})
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'redirectMenuItem')
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      function: redirect,
+      args: [info.linkUrl],
+    })
 })
 
 async function redirect(link) {
   let url = new URL(link)
-  if (!url.pathname.endsWith('.js'))
-    return alert('This is not a JavaScript link')
+  if (!url.pathname.endsWith('.js')) return alert('Not a JavaScript link')
   if (url.host == 'github.com') url = (await fetch(url + '?raw=true')).url
   location.assign('https://jscad.app#' + url)
 }
